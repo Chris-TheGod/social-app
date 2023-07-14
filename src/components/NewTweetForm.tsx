9693f4fad46d8700e8b7f6e38fa51ebb4f43c8e0
Kristjan Lääne <xonproject@gmail.com>
2023-07-14 19:08:56 +0300

    tweet button

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,16 +1,22 @@
-import { Button } from "./Button";
+import { useSession } from 'next-auth/react'
+import { Button } from './Button'
+import { ProfileImage } from './ProfileImage'
 
 export const NewTweetForm = () => {
+  const session = useSession()
+
+  if (session.status !== 'authenticated') return
+
   return (
-    <form className="flex flex-col gap-2 border-b px-4">
-      <div className="flex gap-4">
-        {/* <ProfileImage src="#url" /> */}
+    <form className='flex flex-col gap-2 border-b px-4'>
+      <div className='flex gap-4'>
+        <ProfileImage src={session.data.user.image} />
         <textarea
-          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none'
           placeholder="What's happening?"
         />
       </div>
-      <Button className="self-end">Tweet</Button>
+      <Button className='self-end'>Tweet</Button>
     </form>
-  );
-};
+  )
+}
