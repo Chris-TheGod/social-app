6aee494c5cba1a796c78d9c05623d3cc1f91656b
Kristjan Lääne <xonproject@gmail.com>
2023-07-18 19:15:11 +0300

    tweets show up instantly

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,6 +1,4 @@
 import { useSession } from 'next-auth/react'
-import { Button } from './Button'
-import { ProfileImage } from './ProfileImage'
 import {
   FormEvent,
   useCallback,
@@ -9,29 +7,31 @@ import {
   useState,
 } from 'react'
 import { api } from '~/utils/api'
+import { Button } from './Button'
+import { ProfileImage } from './ProfileImage'
 
-const updateTextAreaSize = (textArea?: HTMLTextAreaElement) => {
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
   if (textArea == null) return
   textArea.style.height = '0'
   textArea.style.height = `${textArea.scrollHeight}px`
 }
 
-export const NewTweetForm = () => {
+export function NewTweetForm() {
   const session = useSession()
   if (session.status !== 'authenticated') return null
 
   return <Form />
 }
 
-export const Form = () => {
+function Form() {
   const session = useSession()
-  const [inputValue, setInputValue] = useState<string>('')
+  const [inputValue, setInputValue] = useState('')
   const textAreaRef = useRef<HTMLTextAreaElement>()
-
   const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
     updateTextAreaSize(textArea)
     textAreaRef.current = textArea
   }, [])
+  const trpcUtils = api.useContext()
 
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaRef.current)
@@ -40,22 +40,49 @@ export const Form = () => {
   const createTweet = api.tweet.create.useMutation({
     onSuccess: (newTweet) => {
       setInputValue('')
-    },
-    onError: (err) => {
-      console.log(err)
+
+      if (session.status !== 'authenticated') return
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return
+
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null,
+          },
+        }
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        }
+      })
     },
   })
 
   if (session.status !== 'authenticated') return null
 
-  const handleSubmit = (e: FormEvent) => {
+  function handleSubmit(e: FormEvent) {
     e.preventDefault()
 
     createTweet.mutate({ content: inputValue })
   }
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-2 border-b px-4'>
+    <form
+      onSubmit={handleSubmit}
+      className='flex flex-col gap-2 border-b px-4 py-2'>
       <div className='flex gap-4'>
         <ProfileImage src={session.data.user.image} />
         <textarea
