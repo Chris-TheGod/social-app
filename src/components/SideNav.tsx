305e77742631937958b87c3765b1db83aefb4357
Kristjan Lääne <xonproject@gmail.com>
2023-07-18 18:17:58 +0300

    hover effects

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,5 +1,7 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
+import { IconHoverEffect } from './IconHoverEffect'
+import { VscHome } from 'react-icons/vsc'
 
 export const SideNav = () => {
   const session = useSession()
@@ -9,11 +11,25 @@ export const SideNav = () => {
     <nav className='sticky top-0 px-2 py-4'>
       <ul className='flex flex-col items-start gap-2 whitespace-nowrap'>
         <li>
-          <Link href='/'>Home</Link>
+          <Link href='/'>
+            <IconHoverEffect>
+              <span className='flex items-center gap-4'>
+                <VscHome className='h-8 w-8' />
+                <span className='hidden text-lg md:inline'>Home</span>
+              </span>
+            </IconHoverEffect>
+          </Link>
         </li>
         {user != null && (
           <li>
-            <Link href={`/profiles/${user.id}`}>Profile</Link>
+            <Link href={`/profiles/${user.id}`}>
+              <IconHoverEffect>
+                <span className='flex items-center gap-4'>
+                  <VscHome className='h-8 w-8' />
+                  <span className='hidden text-lg md:inline'>Home</span>
+                </span>
+              </IconHoverEffect>
+            </Link>
           </li>
         )}
         {user == null ? (
