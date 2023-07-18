ad1663f768611b610cd643de09e4f9763fa38eb8
Kristjan Lääne <xonproject@gmail.com>
2023-07-18 18:50:08 +0300

    like stays

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,9 +1,11 @@
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { ProfileImage } from './ProfileImage'
-import { useSession } from 'next-auth/react'
 import { VscHeart, VscHeartFilled } from 'react-icons/vsc'
 import { IconHoverEffect } from './IconHoverEffect'
+import { api } from '~/utils/api'
+// import { LoadingSpinner } from "./LoadingSpinner";
 
 type Tweet = {
   id: string
@@ -17,7 +19,7 @@ type Tweet = {
 type InfiniteTweetListProps = {
   isLoading: boolean
   isError: boolean
-  hasMore: boolean
+  hasMore: boolean | undefined
   fetchNewTweets: () => Promise<unknown>
   tweets?: Tweet[]
 }
@@ -27,14 +29,15 @@ export function InfiniteTweetList({
   isError,
   isLoading,
   fetchNewTweets,
-  hasMore,
+  hasMore = false,
 }: InfiniteTweetListProps) {
   if (isLoading) return <h1>Laoding...</h1>
   if (isError) return <h1>Error...</h1>
-  if (tweets == null) return null
 
   if (tweets == null || tweets.length === 0) {
-    return <h2 className='my-4 text-center text-2xl text-gray-500'></h2>
+    return (
+      <h2 className='my-4 text-center text-2xl text-gray-500'>No Tweets</h2>
+    )
   }
 
   return (
@@ -43,7 +46,7 @@ export function InfiniteTweetList({
         dataLength={tweets.length}
         next={fetchNewTweets}
         hasMore={hasMore}
-        loader={'Loading...'}>
+        loader={<div>Loading...</div>}>
         {tweets.map((tweet) => {
           return <TweetCard key={tweet.id} {...tweet} />
         })}
@@ -64,6 +67,53 @@ function TweetCard({
   likeCount,
   likedByMe,
 }: Tweet) {
+  const trpcUtils = api.useContext()
+  const toggleLike = api.tweet.toggleLike.useMutation({
+    onSuccess: ({ addedLike }) => {
+      const updateData: Parameters<
+        typeof trpcUtils.tweet.infiniteFeed.setInfiniteData
+      >[1] = (oldData) => {
+        if (oldData == null) return
+
+        const countModifier = addedLike ? 1 : -1
+
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) => {
+            return {
+              ...page,
+              tweets: page.tweets.map((tweet) => {
+                if (tweet.id === id) {
+                  return {
+                    ...tweet,
+                    likeCount: tweet.likeCount + countModifier,
+                    likedByMe: addedLike,
+                  }
+                }
+
+                return tweet
+              }),
+            }
+          }),
+        }
+      }
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, updateData)
+      trpcUtils.tweet.infiniteFeed.setInfiniteData(
+        { onlyFollowing: true },
+        updateData
+      )
+      trpcUtils.tweet.infiniteProfileFeed.setInfiniteData(
+        { userId: user.id },
+        updateData
+      )
+    },
+  })
+
+  function handleToggleLike() {
+    toggleLike.mutate({ id })
+  }
+
   return (
     <li className='flex gap-4 border-b px-4 py-4'>
       <Link href={`/profiles/${user.id}`}>
@@ -82,18 +132,30 @@ function TweetCard({
           </span>
         </div>
         <p className='whitespace-pre-wrap'>{content}</p>
-        <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
+        <HeartButton
+          onClick={handleToggleLike}
+          isLoading={toggleLike.isLoading}
+          likedByMe={likedByMe}
+          likeCount={likeCount}
+        />
       </div>
     </li>
   )
 }
 
 type HeartButtonProps = {
+  onClick: () => void
+  isLoading: boolean
   likedByMe: boolean
   likeCount: number
 }
 
-function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
+function HeartButton({
+  isLoading,
+  onClick,
+  likedByMe,
+  likeCount,
+}: HeartButtonProps) {
   const session = useSession()
   const HeartIcon = likedByMe ? VscHeartFilled : VscHeart
 
@@ -105,8 +167,11 @@ function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
       </div>
     )
   }
+
   return (
     <button
+      disabled={isLoading}
+      onClick={onClick}
       className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${
         likedByMe
           ? 'text-red-500'
