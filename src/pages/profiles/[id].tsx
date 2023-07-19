726bf87489829de27e9e27593caba73548ff9c57
Kristjan Lääne <xonproject@gmail.com>
2023-07-19 17:29:05 +0300

    static pages

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,13 +1,28 @@
-import { GetStaticPaths, GetStaticPropsContext, NextPage } from 'next'
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  NextPage,
+} from 'next'
 import Head from 'next/head'
 import { ssgHelper } from '~/server/api/ssgHelper'
+import { api } from '~/utils/api'
+import ErrorPage from 'next/error'
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  id,
+}) => {
+  const { data: profile } = api.profile.getById.useQuery({ id })
+
+  if (profile == null || profile.name == null)
+    return <ErrorPage statusCode={404} />
+
   return (
     <>
       <Head>
-        <title>{`Twitter Clone ${user.name}`}</title>
+        <title>{`Twitter Clone - ${profile.name}`}</title>
       </Head>
+      {profile.name}
     </>
   )
 }
